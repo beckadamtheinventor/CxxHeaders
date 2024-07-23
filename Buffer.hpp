57fa147f75787896e29422851b6cc8af275a9791
BeckATI on Win10 <40156145+beckadamtheinventor@users.noreply.github.com>
2024-07-23 09:31:49 -0600

    Add and document Buffer.hpp

---
+++ Buffer.hpp
@@ -0,0 +1,130 @@
+/* Simple Read/Write Buffer classes
+ * Author: Adam "beckadamtheinventor" Beckingham
+ * License: MIT
+ */
+#pragma once
+
+template<class T>
+class RWBuffer {
+    protected:
+    T* ptr;
+    size_t len;
+    size_t offset;
+    public:
+    inline RWBuffer<T>(size_t len, size_t offset=0) {
+		if (len == 0) {
+			this->ptr = nullptr;
+		} else {
+			this->ptr = new T[len];
+		}
+        this->len = len;
+        this->offset = offset;
+    }
+    inline RWBuffer<T>(T* ptr, size_t len, size_t offset=0) {
+        this->ptr = ptr;
+        this->len = len;
+        this->offset = offset;
+    }
+    inline bool eof() {
+        return offset >= len || ptr == nullptr;
+    }
+    inline size_t length() {
+        return ptr==nullptr ? 0 : len;
+    }
+    inline size_t available() {
+        return len - offset;
+    }
+    inline bool readable() {
+        return ptr != nullptr;
+    }
+    inline bool writeable() {
+        return ptr != nullptr;
+    }
+    inline void rewind() {
+        offset = 0;
+    }
+	inline size_t tell() {
+		return this->offset;
+	}
+	inline void seek(size_t offset) {
+		this->offset = offset;
+	}
+	inline void seek(size_t offset, int dir) {
+		if (dir == SEEK_START) {
+			this->offset = offset;
+		} else if (dir == SEEK_END) {
+			this->offset = len > offset ? 0 : len - offset;
+		} else if (dir == SEEK_CUR) {
+			this->offset += offset;
+			if (this->offset >= len) {
+				this->offset = len;
+			}
+		}
+	}
+    inline T read() {
+        if (ptr != nullptr && offset + 1 < len) {
+            return ptr[offset++];
+        }
+        return T();
+    }
+    inline bool read(T& v) {
+        if (offset + 1 < len) {
+            v = ptr[offset++];
+            return true;
+        }
+        return false;
+    }
+    size_t read(T* v, size_t amount) {
+        if (ptr == nullptr) {
+			return 0;
+		}
+		if (offset + amount >= len) {
+            amount = len - offset;
+        }
+        for (size_t i=0; i<amount; i++) {
+            v[i] = ptr[offset++];
+        }
+        return amount;
+    }
+    inline bool write(T v) {
+        if (ptr == nullptr) {
+			return false;
+		}
+        if (offset + 1 < len) {
+            ptr[offset++] = v;
+            return true;
+        }
+        return false;
+    }
+    size_t write(T* v, size_t amount) {
+        if (ptr == nullptr) {
+			return 0;
+		}
+        if (offset + amount >= len) {
+            amount = len - offset;
+        }
+        for (size_t i=0; i<amount; i++) {
+            ptr[offset++] = v[i];
+        }
+        return amount;
+    }
+};
+
+template<class T>
+class RBuffer : public RWBuffer<T> {
+    inline bool write(T v) {}
+    inline size_t write(T* v, size_t amount) {}
+    inline bool writeable() {
+        return false;
+    }
+};
+
+template<class T>
+class WBuffer : public RWBuffer<T> {
+    inline T read() {}
+    inline bool read(T& v) {}
+    inline size_t read(T* v, size_t amount) {}
+    inline bool readable() {
+        return false;
+    }
+};
--- README.md
+++ README.md
@@ -55,6 +55,34 @@ Static Functions:
 
 
 
+## Buffer.hpp
+
+Simple read/write buffer classes.
+
+### RWBuffer, RBuffer, WBuffer
+
+Constructors:
++ `RWBuffer<T>(size_t len, size_t offset=0)` Initialize a buffer for reading and writing, optionally with offset.
++ `RWBuffer<T>(T* ptr, size_t len, size_t offset=0)` Initialize a buffer for reading and writing, optionally with offset.
+
+Member Fucntions:
++ `bool eof()` Returns true if the buffer has no more data left to read/write or if the data pointer is null.
++ `size_t length()` Returns the size of the buffer.
++ `size_t available()` Returns the remaining elements in the buffer.
++ `bool readable()` Returns true for RWBuffer and RBuffer classes.
++ `bool writeable()` Returns true for RWBuffer and WBuffer classes.
++ `void rewind()` Rewind the data buffer.
++ `size_t tell()` Return the current offset in the buffer.
++ `void seek(size_t offset)` Seek to an offset in the buffer. (abolute)
++ `void seek(size_t offset, int dir)` Seek to an offset in the buffer. dir must be SEEK_START, SEEK_CUR, or SEEK_END.
++ `T read()` Read one element from the buffer. Use this only if you know there are elements to read; it will error silently otherwise.
++ `bool read(T& v)` Read one element from the buffer, returning true if successful.
++ `size_t read(T* v, size_t amount)` Read amount elements from the buffer, returning the number of elements read.
++ `bool write(T& v)` Write one element to the buffer, returning true if successful.
++ `size_t write(T* v, size_t amount)` Write amount elements to the buffer, returning the number of elements written.
+
+
+
 ## Dictionary.hpp
 
 Simple string keyed dictionary class. Saves keys for later use, ideal for serialization/deserialization.
