289a52c16e7d47bfaa43d7f7816fff76b70df9e2
Adam Beckingham <beckadamtheartist@gmail.com>
2024-08-15 16:10:15 -0600

    Implement basic SparseArray type

---
+++ SparseArray.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "DynamicArray.hpp"
+
+template<class T, size_t CHUNK_SIZE=16, size_t MIN_ALLOC=16>
+class SparseArray {
+    protected:
+    DynamicArray<DynamicArray<T, CHUNK_SIZE>, MIN_ALLOC> _buckets;
+    public:
+    SparseArray<T>() {}
+    SparseArray<T>(T* values, unsigned int count) {
+        for (unsigned int i=0; i<count; i++) {
+            get(i) = values[i];
+        }
+    }
+    T& get(unsigned int i) {
+        return _buckets[i / CHUNK_SIZE][i % CHUNK_SIZE];
+    }
+    unsigned int length() {
+        return _buckets.length()*CHUNK_SIZE;
+    }
+};
